2ad3fb63acdeacd6b1947536aa552a92bb35ada6
David Rometsch <david.rometsch@edu.teko.ch>
2026-05-25 23:22:58 +0200

    feature: persistent menu using statefulWidget

---
+++ app/lib/main_shell.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'screens/easy_222.dart';
+import 'screens/home_screen.dart';
+
+class MainShell extends StatefulWidget {
+  const MainShell({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MainShell> createState() => _MainShellState();
+}
+
+class _MainShellState extends State<MainShell> {
+  Widget currentScreen = HomeScreen(title: 'Home');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(child: Text('Menu')),
+
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                setState(() {
+                  currentScreen = HomeScreen(title: 'Home');
+                });
+              },
+            ),
+
+            ListTile(
+              title: Text('Easy 222'),
+              onTap: () {
+                setState(() {
+                  currentScreen = EasyTwo(title: 'Easy Two');
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+
+      body: currentScreen,
+    );
+  }
+}
