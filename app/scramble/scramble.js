e88b52968cd41018e54f51d1993591c734c725a9
David Rometsch <david.rometsch@edu.teko.ch>
2026-05-31 23:07:40 +0200

    feature: rotate before and after scrambling

---
+++ app/scramble/scramble.js
@@ -0,0 +1,103 @@
+import { experimentalSolve2x2x2 } from "cubing/search";
+import { puzzles } from "cubing/puzzles"
+import { KPattern } from "cubing/kpuzzle";
+import { scrambleTypes } from "./scramble-definition.js";
+import { rotate, rotateScramble } from "./rotate.js";
+
+let scrambleType = 'threeOriented';
+let direction = 'x';
+
+
+export let solvedCubeState = {
+  0: { p: 0, o: 0 }, //UFR
+  1: { p: 1, o: 0 }, //UBR
+  2: { p: 2, o: 0 }, //UBL
+  3: { p: 3, o: 0 }, //UFL
+  4: { p: 4, o: 0 }, //DFR
+  5: { p: 5, o: 0 }, //DFL
+  6: { p: 6, o: 0 }, //DBL
+  7: { p: 7, o: 0 }, //DBR 
+}
+
+
+export function permute(cubeState, orbit) {
+  for (let i = orbit.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cubeState[orbit[i]], cubeState[orbit[j]]] = [cubeState[orbit[j]], cubeState[orbit[i]]];
+  }
+}
+
+
+export function orient(cubeState, orbit) {
+
+  for (let i = 0; i < orbit.length; i++) {
+    const j = Math.floor(Math.random() * 3);
+    cubeState[orbit[i]].o = j;
+  }
+}
+
+
+export function fixPairity(cubeState, ollOrbit, balancePiece) {
+  if (ollOrbit.length === 0) return;
+  let counter = 0;
+  for (let i of Object.keys(cubeState)) {
+    if (parseInt(i) !== balancePiece) {
+      counter += cubeState[i].o;
+    }
+  }
+  cubeState[balancePiece].o = (3 - counter % 3) % 3;
+}
+
+
+export function cubeStateToK(cubeState) {
+  const pieces = [];
+  const orientation = [];
+
+  for (const [place, data] of Object.entries(cubeState)) {
+    pieces[parseInt(place)] = data.p;
+    orientation[parseInt(place)] = data.o;
+  }
+  return { CORNERS: { pieces: pieces, orientation: orientation } }
+}
+
+
+export async function scramble(scrambleType) {
+
+  let scrambleDef = scrambleTypes[scrambleType];
+  let cubeState = structuredClone(solvedCubeState);
+
+
+  cubeState = rotate(cubeState, direction);
+  // console.log(JSON.stringify(scrambleDef));
+
+
+  let orbit1 = scrambleDef.orbit1;
+  let orbit2 = scrambleDef.orbit2;
+  let ollOrbit = scrambleDef.ollOrbit;
+  let balancePiece = scrambleDef.balancePiece;
+
+
+  permute(cubeState, orbit1);
+  permute(cubeState, orbit2);
+  orient(cubeState, ollOrbit);
+  fixPairity(cubeState, ollOrbit, balancePiece);
+
+  console.log(JSON.stringify(cubeState));
+
+  var KCubeState = cubeStateToK(cubeState);
+
+  console.log(`kcubestate: ${JSON.stringify(KCubeState)}`);
+  const cube2x2 = await puzzles["2x2x2"].kpuzzle();
+  let scrState = new KPattern(cube2x2, KCubeState);
+  // rotade randomley
+  scrState = rotateScramble(cube2x2, scrState);
+
+  const solution = await experimentalSolve2x2x2(scrState);
+  const scramble = solution.invert();
+  return scramble;
+}
+
+
+if (process.argv[1].endsWith("scramble.js")) {
+  scramble(scrambleType).then((s) => console.log(s.toString()));
+}
---
+++ app/scramble/test.js
@@ -0,0 +1,48 @@
+import { puzzles } from "cubing/puzzles";
+import { rotate } from "./rotate.js";
+import { experimentalSolve2x2x2 } from "cubing/search";
+import { KPattern } from "cubing/kpuzzle";
+
+import { permute, orient, solvedCubeState, fixPairity, cubeStateToK} from './scramble.js';
+import { scrambleTypes } from './scramble-definition.js';
+
+
+//
+//
+let scrambleType = 'pll';
+// let axis = 'redOrange';
+//
+let orbit1 = scrambleTypes[scrambleType].orbit1;
+// let orbit2 = scrambleTypes[scrambleType].orbit2;
+let ollOrbit = scrambleTypes[scrambleType].ollOrbit;
+//
+// let scrambledOrbit1 = permute(orbit1);
+// // let scrambledOrbit2 = Object.fromEntries(orbit2.map(v => [v, v]));
+// let bothOrbits = { ...scrambledOrbit1, ...scrambledOrbit2 };
+//
+//
+export async function test(scrambleType) {
+ 
+  let myCubeState = structuredClone(solvedCubeState);
+  
+  
+  myCubeState = rotate(myCubeState, 'x');
+  
+  // console.log(JSON.stringify(myCubeState));
+
+  // permute(myCubeState, orbit1);
+  // orient(myCubeState, ollOrbit);
+  // fixPairity(myCubeState, scrambleTypes[scrambleType].balancePiece);
+  var KCubeState = cubeStateToK(myCubeState);
+  // console.log(`k cube state: ${JSON.stringify(KCubeState)}`);
+
+  const cube2x2 = await puzzles["2x2x2"].kpuzzle();
+  const scrState = new KPattern(cube2x2, KCubeState);
+  const solution = await experimentalSolve2x2x2(scrState);
+  const scramble = solution.invert();
+  return scramble;
+}
+if (process.argv[1].endsWith("test.js")) {
+  test(scrambleType).then((s) => console.log(s.toString()));
+}
+
