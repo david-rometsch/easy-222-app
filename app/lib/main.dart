a6abf2e35ec5adeecbf85163853817e225db07f9
David Rometsch <david.rometsch@edu.teko.ch>
2026-06-22 15:09:12 +0200

    feat: add hive-db

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 import 'main_shell.dart';
 import 'package:app/app_theme.dart';
 import 'package:app/app_locale.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-
-void main() {
+void main() async {
   // debugPaintSizeEnabled = true;
+  WidgetsFlutterBinding.ensureInitialized(); // makes sure fltter is loaded at this point
+  await Hive.initFlutter(); // init hive incl path
+
+  final hive = await Hive.openBox('settings');
+
+  if (!hive.containsKey('first_start')) {
+    hive.put('wca_id', null);
+    hive.put('first_start', true);
+  }
+
   runApp(
     AppLocale(
       notifier: localeNotfifier,
@@ -14,10 +25,8 @@ void main() {
         theme: AppTheme.light,
         darkTheme: AppTheme.dark,
         themeMode: ThemeMode.system,
-        home: const MainShell()
-      )
-    )
+        home: const MainShell(),
+      ),
+    ),
   );
 }
-
-
