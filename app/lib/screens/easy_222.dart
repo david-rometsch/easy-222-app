fea46ff186d5c412ffff363f47de2d870e992f05
David Rometsch <david.rometsch@edu.teko.ch>
2026-06-27 23:22:59 +0200

    feat: add timer

--- app/lib/screens/easy_222.dart
+++ app/lib/screens/easy_222.dart
@@ -4,6 +4,7 @@ import 'package:app/get_scramble.dart';
 import 'package:app/app_settings.dart';
 import 'package:shake/shake.dart';
 import 'package:app/app_locale.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class EasyTwo extends StatefulWidget {
   const EasyTwo({super.key});
@@ -12,15 +13,29 @@ class EasyTwo extends StatefulWidget {
   State<StatefulWidget> createState() => _EasyTwoState();
 }
 
+enum TimerPhase { idle, solving }
+
 class _EasyTwoState extends State<EasyTwo> {
   final _scrambler = GetScramble();
   String _scramble = '';
   late ShakeDetector _shakeDetector;
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer();
+  TimerPhase _timerPhase = TimerPhase.idle;
 
   Future<void> _nextScramble() async {
     final s = await _scrambler.pickRandomScramble();
     setState(() => _scramble = s);
   }
+
+  String formatTime(double sec) {
+    final hr = sec /~ 3600;
+    final min = (sec % 3600) /~ 60;
+    final s= sec % 60;  
+
+    if (hr > 0) return '$hr:$min:$s';
+    if (min > 0) return '$min:$s';
+    return '$s';
+  } 
 
   @override
   void initState() {
@@ -75,8 +90,9 @@ class _EasyTwoState extends State<EasyTwo> {
 
   @override
   void dispose() {
-    _shakeDetector.stopListening();
     super.dispose();
+    _shakeDetector.stopListening();
+    _stopWatchTimer.dispose();
   }
 
   Future<void> _init() async {
@@ -98,20 +114,56 @@ class _EasyTwoState extends State<EasyTwo> {
         return KeyEventResult.ignored;
       },
       child: GestureDetector(
-        onTap: _nextScramble,
+        onTap: () {
+          switch (_timerPhase) {
+            case TimerPhase.idle:
+              _stopWatchTimer.onStartTimer();
+              _timerPhase = TimerPhase.solving;
+            case TimerPhase.solving:
+              _stopWatchTimer.onStopTimer();
+              _timerPhase = TimerPhase.idle;
+              _nextScramble();
+          }
+        },
         child: Container(
           color: Colors.blue,
-          child: Center(
-            child: _scramble.isEmpty
-                ? CircularProgressIndicator()
-                : Text(
-                    _scramble,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+          child: Column(
+            children: [
+              Center(
+                child: _scramble.isEmpty
+                    ? CircularProgressIndicator()
+                    : Text(
+                        _scramble,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+              ),
+              StreamBuilder<int>(
+                stream: _stopWatchTimer.rawTime,
+                initialData: 0,
+                builder: (context, snap) {
+                  final rawTime = snap.data ?? 0;
+                  // final displayTime = StopWatchTimer.getDisplayTime(value);
+                  return Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          formatTime(rawTime / 1000),
+                          style: const TextStyle(
+                            fontFamily: 'monospace',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
